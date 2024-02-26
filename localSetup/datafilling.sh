5d62eeaf152233a6737c4ce1472b7e8123f5c9e6
prince.priyadarshi <prince.priyadarshi@agilonhealth.com>
2024-02-26 16:26:48 +0530

    .sh was not running

--- localSetup/datafilling.sh
+++ localSetup/datafilling.sh
@@ -5,18 +5,11 @@ DOCKER_CONTAINER_NAME="my-mysql"
 MYSQL_USER="root"
 MYSQL_PASSWORD="admin"
 
-# Create database and table
-docker exec $DOCKER_CONTAINER_NAME mysql -u $MYSQL_USER -p$MYSQL_PASSWORD << EOF
-CREATE DATABASE IF NOT EXISTS products;
-USE products;
-CREATE TABLE IF NOT EXISTS product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    service VARCHAR(255) NOT NULL,
-    netprice INT NOT NULL,
-    mrp INT NOT NULL,
-    created DATETIME
-);
-EOF
+# Create database
+docker exec $DOCKER_CONTAINER_NAME mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS products;"
+
+# Create table
+docker exec $DOCKER_CONTAINER_NAME mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "USE products; CREATE TABLE IF NOT EXISTS product (id BIGINT AUTO_INCREMENT PRIMARY KEY, service VARCHAR(255) NOT NULL, netprice INT NOT NULL, mrp INT NOT NULL, created DATETIME);"
 
 # Insert 100 dummy rows
 for i in {1..100}
@@ -24,7 +17,7 @@ do
     SERVICE="Service$i"
     NETPRICE=$((RANDOM % 100 + 1))
     MRP=$((NETPRICE + (RANDOM % 50)))
-    docker exec $DOCKER_CONTAINER_NAME mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -D products -e "INSERT INTO product (service, netprice, mrp, created) VALUES ('$SERVICE', $NETPRICE, $MRP, NOW());"
+    docker exec $DOCKER_CONTAINER_NAME mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "USE products; INSERT INTO product (service, netprice, mrp, created) VALUES ('$SERVICE', $NETPRICE, $MRP, NOW());"
 done
 
-echo "Database 'product' created with table 'product' and 100 dummy rows inserted."
+echo "Database 'products' created with table 'product' and 100 dummy rows inserted."
